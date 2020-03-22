b360305aef4bddf6ff3b441fc80858c2ddb55f7d
Vicky Hermawan <hv3344588@gmail.com>
2020-03-22 21:43:51 +0700

    Updates

---
+++ precache-manifest.bd18b7e86aa07f2b1e0cdfb0a218511a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "187dc102efd341e1fa6268397bcdb3fc",
+    "url": "/MateriWeb3/index.html"
+  },
+  {
+    "revision": "b64dceca55853f6b49c5",
+    "url": "/MateriWeb3/static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "76dc64e9c6eb2867c215",
+    "url": "/MateriWeb3/static/css/main.fcd27899.chunk.css"
+  },
+  {
+    "revision": "b64dceca55853f6b49c5",
+    "url": "/MateriWeb3/static/js/2.b8253ffb.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/MateriWeb3/static/js/2.b8253ffb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76dc64e9c6eb2867c215",
+    "url": "/MateriWeb3/static/js/main.80504555.chunk.js"
+  },
+  {
+    "revision": "13ac7a6f3df1a71390db",
+    "url": "/MateriWeb3/static/js/runtime-main.e851b085.js"
+  },
+  {
+    "revision": "e834ca3ae54d9ed6a644eea76fd7fbbb",
+    "url": "/MateriWeb3/static/media/picture.e834ca3a.svg"
+  }
+]);
